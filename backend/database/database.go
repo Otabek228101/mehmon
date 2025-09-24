1af9b8c5b258b1382881178d558150b5c7d13688
Otabek228101 <bekotabekbek56@gmail.com>
2025-09-24 19:48:09 +0500

    .

--- backend/database/database.go
+++ backend/database/database.go
@@ -52,16 +52,29 @@ func SeedData() {
 
 	if hotelCount == 0 {
 		hotels := []models.Hotel{
-			{Name: "Violino d'Oro", City: "Venezia", Type: "Luxury", Address: "P.za San Marco, 2091, 30124 Venezia VE", Stars: 5, Breakfast: true},
-			{Name: "UNA Hotels Regina Bari", City: "BARI", GroupName: "UNA Hotels", Type: "Business", Address: "SP57 Torre a Mare / Noicattaro, Noicattaro (BA)", Stars: 4, Breakfast: true},
-			{Name: "UNA Hotels Bologna Centro", City: "BOLOGNA", GroupName: "UNA Hotels", Type: "Business", Address: "Viale Pietro Pietramellara, 41, Bologna", Stars: 4, Breakfast: true},
-			{Name: "L'HERMITAGE", City: "LA BAULE", GroupName: "Barriere Group", Type: "Luxury", Address: "5 Espl. Lucien Barrière, 44500 La Baule-Escoublac, France", Stars: 5, Breakfast: true},
-			{Name: "Principi di Piemonte | UNA Esperienze", City: "TORINO", GroupName: "UNA Hotels", Type: "Luxury", Address: "Via Piero Gobetti, 15, 10123 Torino TO", Stars: 5, Breakfast: true},
-			{Name: "Le Majestic", City: "CANNES", GroupName: "Barriere Group", Type: "Luxury", Address: "10 Bd de la Croisette, Cannes", Stars: 5, Breakfast: true},
-			{Name: "Le Gray D'Albion", City: "CANNES", GroupName: "Barriere Group", Type: "Business", Address: "38 Rue des Serbes, Cannes", Stars: 4, Breakfast: true},
-			{Name: "Grand Hotel Minerva", City: "FLORENCE", GroupName: "Collezione EM", Type: "Boutique", Address: "P.za di Santa Maria Novella, 16, 50123 Firenze FI", Stars: 4, Breakfast: true},
-			{Name: "Brunelleschi Firenze", City: "FLORENCE", GroupName: "Collezione EM", Type: "Boutique", Address: "Piazza Sant'Elisabetta, 3, 50122 Firenze FI", Stars: 4, Breakfast: true},
-			{Name: "Pensione America", City: "FORTE DEI MARMI", GroupName: "Collezione EM", Type: "Luxury", Address: "Via Colombo, 24, 55042 Forte dei Marmi LU, Italy", Stars: 5, Breakfast: true},
+			// UNA Hotels
+			{Name: "UNA Hotels Regina Bari", City: "BARI", GroupName: "UNA Hotels", Type: "Business", Address: "SP57 Torre a Mare / Noicattaro, Noicattaro (BA)", Stars: 4, Breakfast: true, WebsiteLink: "https://booking.unaitalianhospitality.com/?adult=1&arrive=2025-06-05&chain=33116&child=0&depart=2025-06-06&level=chain&locale=it-IT&rooms=1", LocationLink: "https://goo.gl/maps/example1"},
+			{Name: "UNA Hotels Bologna Centro", City: "BOLOGNA", GroupName: "UNA Hotels", Type: "Business", Address: "Viale Pietro Pietramellara, 41, Bologna", Stars: 4, Breakfast: true, WebsiteLink: "https://booking.unaitalianhospitality.com/?adult=1&arrive=2025-06-05&chain=33116&child=0&depart=2025-06-06&level=chain&locale=it-IT&rooms=1", LocationLink: "https://goo.gl/maps/example2"},
+			{Name: "Principi di Piemonte | UNA Esperienze", City: "TORINO", GroupName: "UNA Hotels", Type: "Luxury", Address: "Via Piero Gobetti, 15, 10123 Torino TO", Stars: 5, Breakfast: true, WebsiteLink: "https://booking.unaitalianhospitality.com/?adult=1&arrive=2025-06-05&chain=33116&child=0&depart=2025-06-06&level=chain&locale=it-IT&rooms=1", LocationLink: "https://goo.gl/maps/example3"},
+			{Name: "UNA Hotels Varese", City: "VARESE", GroupName: "UNA Hotels", Type: "Business", Address: "Via Francesco Albani, 73, Varese", Stars: 4, Breakfast: true, WebsiteLink: "https://booking.unaitalianhospitality.com/?adult=1&arrive=2025-06-05&chain=33116&child=0&depart=2025-06-06&level=chain&locale=it-IT&rooms=1", LocationLink: "https://goo.gl/maps/example4"},
+			{Name: "UNA Hotels Bologna Fiera", City: "BOLOGNA", GroupName: "UNA Hotels", Type: "Business", Address: "Piazza del Costituzione, 1, Bologna", Stars: 4, Breakfast: true, WebsiteLink: "https://booking.unaitalianhospitality.com/?adult=1&arrive=2025-06-05&chain=33116&child=0&depart=2025-06-06&level=chain&locale=it-IT&rooms=1", LocationLink: "https://goo.gl/maps/example5"},
+			{Name: "UNA Hotels Bologna San Lazzaro", City: "BOLOGNA", GroupName: "UNA Hotels", Type: "Business", Address: "Via Luigi Fantini, 1, Bologna", Stars: 4, Breakfast: true, WebsiteLink: "https://booking.unaitalianhospitality.com/?adult=1&arrive=2025-06-05&chain=33116&child=0&depart=2025-06-06&level=chain&locale=it-IT&rooms=1", LocationLink: "https://goo.gl/maps/example6"},
+
+			// Barriere Group
+			{Name: "L'HERMITAGE", City: "LA BAULE", GroupName: "Barriere Group", Type: "Luxury", Address: "5 Espl. Lucien Barrière, 44500 La Baule-Escoublac, France", Stars: 5, Breakfast: true, WebsiteLink: "https://reservations.hotelsbarriere.com/?adult=2&arrive=2025-06-05&chain=32004&child=0&currency=EUR&depart=2025-06-06&level=hotel&locale=en-US&rooms=1", LocationLink: "https://goo.gl/maps/example7"},
+			{Name: "Le Majestic", City: "CANNES", GroupName: "Barriere Group", Type: "Luxury", Address: "10 Bd de la Croisette, Cannes", Stars: 5, Breakfast: true, WebsiteLink: "https://reservations.hotelsbarriere.com/?adult=2&arrive=2025-06-05&chain=32004&child=0&currency=EUR&depart=2025-06-06&level=hotel&locale=en-US&rooms=1", LocationLink: "https://goo.gl/maps/example8"},
+			{Name: "Le Gray D'Albion", City: "CANNES", GroupName: "Barriere Group", Type: "Business", Address: "38 Rue des Serbes, Cannes", Stars: 4, Breakfast: true, WebsiteLink: "https://reservations.hotelsbarriere.com/?adult=2&arrive=2025-06-05&chain=32004&child=0&currency=EUR&depart=2025-06-06&level=hotel&locale=en-US&rooms=1", LocationLink: "https://goo.gl/maps/example9"},
+
+			// Collezione EM
+			{Name: "Violino d'Oro", City: "VENEZIA", GroupName: "Violino d'Oro", Type: "Luxury", Address: "P.za San Marco, 2091, 30124 Venezia VE", Stars: 5, Breakfast: true, WebsiteLink: "https://violinodoro.com/", LocationLink: "https://goo.gl/maps/example10"},
+			{Name: "Grand Hotel Minerva", City: "FLORENCE", GroupName: "Collezione EM", Type: "Boutique", Address: "P.za di Santa Maria Novella, 16, 50123 Firenze FI", Stars: 4, Breakfast: true, WebsiteLink: "https://grandhotelminerva.com/", LocationLink: "https://goo.gl/maps/example11"},
+			{Name: "Brunelleschi Firenze", City: "FLORENCE", GroupName: "Collezione EM", Type: "Boutique", Address: "Piazza Sant'Elisabetta, 3, 50122 Firenze FI", Stars: 4, Breakfast: true, WebsiteLink: "https://www.brunelleschihotelflorence.com/", LocationLink: "https://goo.gl/maps/example12"},
+			{Name: "Pensione America", City: "FORTE DEI MARMI", GroupName: "Collezione EM", Type: "Luxury", Address: "Via Colombo, 24, 55042 Forte dei Marmi LU, Italy", Stars: 5, Breakfast: true, WebsiteLink: "https://www.pensioneamerica.com/", LocationLink: "https://goo.gl/maps/example13"},
+
+			// Дополнительные отели для разнообразия
+			{Name: "UNA Hotels Napoli", City: "NAPOLI", GroupName: "UNA Hotels", Type: "Business", Address: "Piazza Giuseppe Garibaldi, 9, Napoli", Stars: 4, Breakfast: true, WebsiteLink: "https://booking.unaitalianhospitality.com/?adult=1&arrive=2025-06-05&chain=33116&child=0&depart=2025-06-06&level=chain&locale=it-IT&rooms=1", LocationLink: "https://goo.gl/maps/example14"},
+			{Name: "UNA Hotels Milano", City: "MILAN", GroupName: "UNA Hotels", Type: "Business", Address: "Via Carlo de Cristoforis, 6, Milano", Stars: 4, Breakfast: true, WebsiteLink: "https://booking.unaitalianhospitality.com/?adult=1&arrive=2025-06-05&chain=33116&child=0&depart=2025-06-06&level=chain&locale=it-IT&rooms=1", LocationLink: "https://goo.gl/maps/example15"},
+			{Name: "Le Normandy", City: "DEAUVILLE", GroupName: "Barriere Group", Type: "Luxury", Address: "38 Rue Jean Mermoz, Deauville", Stars: 5, Breakfast: true, WebsiteLink: "https://reservations.hotelsbarriere.com/?adult=2&arrive=2025-06-05&chain=32004&child=0&currency=EUR&depart=2025-06-06&level=hotel&locale=en-US&rooms=1", LocationLink: "https://goo.gl/maps/example16"},
 		}
 
 		for _, hotel := range hotels {
@@ -164,6 +177,32 @@ func SeedData() {
 		}
 
 		log.Println("Test receipts and activities seeded successfully")
+	}
+
+	if proposalCount == 0 {
+		proposals := []models.Proposal{
+			{
+				ProposalNumber: "P00001",
+				ClientName:     "Test Client",
+				Guests:         2,
+				Nights:         3,
+				NumberOfRooms:  1,
+				RoomType:       "Standard",
+				CheckIn:        time.Now(),
+				CheckOut:       time.Now().Add(72 * time.Hour),
+				Breakfast:      true,
+				FreeCancel:     true,
+				Price:          450.0,
+				HotelID:        1,
+			},
+		}
+
+		for _, proposal := range proposals {
+			if err := DB.Create(&proposal).Error; err != nil {
+				log.Printf("Failed to create proposal %s: %v", proposal.ProposalNumber, err)
+			}
+		}
+		log.Println("Proposals seeded successfully")
 	}
 
 	log.Println("Database seeding completed")
